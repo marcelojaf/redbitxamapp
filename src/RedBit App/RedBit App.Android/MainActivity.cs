67d3e236dd31b942598121a6eeb165ac727d98a6
Marcelo Amador <marcelo@redbitdev.com>
2022-02-07 17:38:20 -0500

    App is running with Plugin.Firebase installed

---
+++ src/RedBit App/RedBit App.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Firebase;
+using Plugin.FirebasePushNotification;
+
+namespace RedBit_App.Droid
+{
+    [Activity(Label = "RedBit_App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FirebaseApp.InitializeApp(this);
+
+            LoadApplication(new App());
+            FirebasePushNotificationManager.ProcessIntent(this, Intent);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
